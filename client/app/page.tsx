5364f43cb25b97d49691c6f9085f062bf1f01e41
Ajay <ajayramsaravanan4@gmail.com>
2023-09-18 12:27:42 +0530

    :sparkles: Integrate API with Frontend

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,25 @@
-"use client";
+import { redirect } from "next/navigation";
 
 const page = () => {
-  const handleSubmit = () => {
-    alert("Thank you for your msg");
+  const handleSubmit = async (formData: FormData) => {
+    "use server";
+    if (!formData.get("message")) return;
+    const data = { message: formData.get("message") };
+    console.log(data);
+    const res = await fetch(`${process.env.API_URL}/addmsg`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const json = await res.json();
+    if (json.status == "success") {
+      console.log("success");
+      redirect("/thankyou");
+    } else {
+      console.log("error");
+    }
   };
   return (
     <div className=" w-full h-screen flex items-center justify-center relative">
@@ -13,7 +30,10 @@ const page = () => {
             Anonymously
           </div>
         </div>
-        <form className=" w-full h-full flex flex-col items-center justify-center">
+        <form
+          className=" w-full h-full flex flex-col items-center justify-center"
+          action={handleSubmit}
+        >
           <label className=" flex flex-col w-full h-5/6 md:gap-6 gap-4">
             <div className=" flex flex-col md:gap-2">
               <div className="md:text-3xl text-lg font-semibold">
@@ -28,11 +48,12 @@ const page = () => {
             <textarea
               className=" md:w-full md:h-full h-48 rounded-2xl bg-neutral-800 p-4 focus:outline-purple-400 outline-none "
               placeholder="Your message"
+              name="message"
             />
           </label>
           <button
             className=" px-4 py-2 w-fit bg-purple-400 self-end my-4 rounded-xl"
-            onClick={handleSubmit}
+            type="submit"
           >
             Send
           </button>
