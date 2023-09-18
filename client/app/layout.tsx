e2cf273bbe84a6f612416d164e4f4b4e3ba59d0f
Ajay <ajayramsaravanan4@gmail.com>
2023-09-18 23:44:05 +0530

    :sparkles: Add OG Image

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,6 +7,27 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Msg Ajay 😎",
   description: "Msg me anonymously",
+  openGraph: {
+    title: "Message Ajay 😎",
+    description: "Anonymously message mee",
+    url: "https://anonymous.ajrm.xyz/",
+    siteName: "Msg Ajay 😎",
+    images: [
+      {
+        url: "https://i.seadn.io/gae/hsWaOw_SOTvLES83mRykzKjaSn2Duzna0HVkXyLbkhekTprkpjeYXhq2bCUc4vy_fKzBEEFbgwSK9z-GAFT2riLM-3t4DI96dYejfQ?auto=format&dpr=1&w=1000",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://i.seadn.io/gae/hsWaOw_SOTvLES83mRykzKjaSn2Duzna0HVkXyLbkhekTprkpjeYXhq2bCUc4vy_fKzBEEFbgwSK9z-GAFT2riLM-3t4DI96dYejfQ?auto=format&dpr=1&w=1000",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
